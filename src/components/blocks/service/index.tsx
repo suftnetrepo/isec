dc1d0f76723f43ef729a0530c64ec5278dce84c4
suftnetrepo <pr@suftnet.com>
2026-02-18 18:10:44 +0000

    Added new features, pages and fix issues

--- src/components/blocks/service/index.tsx
+++ src/components/blocks/service/index.tsx
@@ -7,7 +7,6 @@ export function Services({ showTitle = true }) {
       {showTitle && (
         <div className="row text-center mb-10">
           <div className="col-md-10 offset-md-1 col-lg-8 offset-lg-2">
-            <h2 className="fs-16 text-uppercase text-gradient gradient-1 mb-3">Our Services</h2>
             <h3 className="fs-24 lead mb-9 px-xl-11">We deliver fully integrated fire and security solutions — from concept to long-term support — ensuring continuous protection for what matters most.</h3>
           </div>
         </div>
